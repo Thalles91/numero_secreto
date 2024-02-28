9edd8f3d23f8ca5a293c9e0e0c915c49c138e909
Thalles Gonçalves <thallesprofissionalti@gmail.com>
2024-02-28 16:05:21 -0300

    projeto_inicial

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -15,7 +15,7 @@ function exibirMensagemInicial() {
 }
 
 exibirMensagemInicial();
-
+/*novo comentario*/
 function verificarChute() {
     let chute = document.querySelector('input').value;
     
